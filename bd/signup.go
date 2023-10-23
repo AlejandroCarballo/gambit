1ea78b097372bd79e68bc5bcd2dd139784f7a768
AlejandroCarballo <o.alejandro.carballo@gmail.com>
2023-10-23 20:18:13 -0300

    Ultimo commit

---
+++ bd/signup.go
@@ -0,0 +1,28 @@
+package bd
+
+import (
+	"fmt"
+	"gambit/models"
+	"gambit/tools"
+)
+
+func SignUp(sig models.SignUp) error {
+	println("Comienza registro")
+	err := DbConnect()
+
+	if err != nil {
+		return err
+	}
+
+	defer Db.Close()
+
+	setencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySql() + "')"
+	fmt.Println(setencia)
+
+	_, err = Db.Exec(setencia)
+	if err != nil {
+		fmt.Println(err.Error())
+
+	}
+	return nil
+}
